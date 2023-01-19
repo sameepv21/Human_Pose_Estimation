b80a2cfabe2162c963b543d5bd7e1d76aace6d0e
sameepv21 <sameep0112vani@gmail.com>
2023-01-19 16:57:15 +0530

    chore: removed unwanted terminal outputs

--- scripts/large.py
+++ scripts/large.py
@@ -8,7 +8,8 @@ VAL_URL = 'http://images.cocodataset.org/zips/val2017.zip'
 
 def download_train():
     os.system('curl -L -o "train2017.zip" ' + TRAIN_URL)
-    os.system('unzip train2017.zip')
+    print("EXTRACTING TRAINING SET.....")
+    os.system('unzip train2017.zip >/dev/null 2')
     os.system('rm -rf train2017.zip')
     os.system('cp train2017 ../data/train2017 -r')
     os.system('rm -rf train2017/')
@@ -17,7 +18,8 @@ def download_train():
 
 def download_test():
     os.system('curl -L -o "test2017.zip" ' + TEST_URL)
-    os.system('unzip test2017.zip')
+    print("EXTRACTING TEST SET.....")
+    os.system('unzip test2017.zip >/dev/null 2')
     os.system('rm -rf test2017.zip')
     os.system('cp test2017 ../data/test2017 -r')
     os.system('rm -rf test2017/')
@@ -26,7 +28,8 @@ def download_test():
 
 def download_val():
     os.system('curl -L -o "val2017.zip" ' + VAL_URL)
-    os.system('unzip val2017.zip')
+    print("EXTRACTING VALIDATION SET.....")
+    os.system('unzip val2017.zip >/dev/null 2')
     os.system('rm -rf val2017.zip')
     os.system('cp val2017 ../data/val2017 -r')
     os.system('rm -rf val2017/')
--- scripts/small.py
+++ scripts/small.py
@@ -7,8 +7,10 @@ URL = "https://drive.google.com/file/d/1lwt3smqdJ2-ZuVCzgImEp8gw-RHuG-YR/view"
 OUTPUT_FILE_NAME = "dataset.zip"
 gdown.download(URL, OUTPUT_FILE_NAME, quiet = False, fuzzy = True)
 
-# Extract the zip folder and place it in the dataset folder
-os.system("unzip dataset.zip")
+# Extract the zip folder and place it in the data folder
+print("EXTRACTING.....")
+os.system("unzip dataset.zip >/dev/null 2")
 os.system('rm -rf dataset.zip')
 os.system('cp coco_single_person_only ../data/small  -r')
 os.system('rm -rf coco_single_person_only/')
+print("COMPLETED EXTRACTION")
