8e3e7886b4adfe9cb734aadcffd1cad086a19624
sameepv21 <sameep0112vani@gmail.com>
2023-01-19 14:15:42 +0530

    bug: cant create a directory

---
+++ scripts/small.py
@@ -0,0 +1,16 @@
+# Import libraries
+import gdown
+import os
+
+# Required variables
+URL = "https://drive.google.com/file/d/1lwt3smqdJ2-ZuVCzgImEp8gw-RHuG-YR/view"
+OUTPUT_FILE_NAME = "dataset.zip"
+gdown.download(URL, OUTPUT_FILE_NAME, quiet = False, fuzzy = True)
+
+# Extract the zip folder and place it in the dataset folder
+os.system("unzip dataset.zip")
+os.system('rm -rf dataset.zip')
+os.system('cp coco_single_person_only small  -r')
+os.system('rm -rf coco_single_person_only')
+os.system('cp small Human_Pose_Estimation/data -r')
+os.system('rm -rf small/')
