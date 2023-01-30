0642a5945ebc47aa9a16a2dd8dc65e4889329692
sameepv21 <sameep0112vani@gmail.com>
2023-01-30 13:12:43 +0530

    added: split-folders library

---
+++ scripts/prepare_data.py
@@ -0,0 +1,15 @@
+# Required libraries
+import numpy as np
+import pandas as pd
+import os
+import shutil
+
+# Global variables
+PERCENTAGE_TEST_SIZE = 20
+RELATIVE_PATH = '../data'
+
+# function to get image names in dataframe
+def generate_df():
+    df = pd.DataFrame()
+        
+    return df
