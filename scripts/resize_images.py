e0495e07a2876fa4be22add66affde4e190836b4
sameepv21 <sameep0112vani@gmail.com>
2023-01-30 17:00:12 +0530

    chore: added time taken and assertion after every 4000 images

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -1,7 +1,9 @@
 # Required libraries
-import numpy as np
+import time
 from PIL import Image
 import os
+
+start = time.time()
 
 # Global variables
 TRAIN_PATH = "../data/train/images"
@@ -11,7 +13,9 @@ SCALING_SIZE = (224, 224)
 
 print('Processing Train images.....')
 # Load and resize train image
-for fileName in os.listdir(TRAIN_PATH):
+for index, fileName in enumerate(os.listdir(TRAIN_PATH)):
+    if index % 4000 == 0:
+        print('Processed', index, 'images')
     _image = Image.open(TRAIN_PATH + '/' + fileName)
     _image = _image.resize(SCALING_SIZE)
     os.system("rm -rf " + os.path.join(TRAIN_PATH, fileName))
@@ -21,7 +25,9 @@ print('Completed Train images.....')
 
 print('\nProcessing Test images.....')
 # Load and resize test image
-for fileName in os.listdir(TEST_PATH):
+for index, fileName in enumerate(os.listdir(TEST_PATH)):
+    if index % 4000 == 0:
+        print('Processed', index, 'images')
     _image = Image.open(TEST_PATH + '/' + fileName)
     _image = _image.resize(SCALING_SIZE)
     os.system("rm -rf " + os.path.join(TEST_PATH, fileName))
@@ -31,9 +37,14 @@ print('Completed Test images.....')
 
 print('\nProcessing Validation images.....')
 # Load and resize validation image
-for fileName in os.listdir(VAL_PATH):
+for index, fileName in enumerate(os.listdir(VAL_PATH)):
+    if index % 4000 == 0:
+        print('Processed', index, 'images')
     _image = Image.open(VAL_PATH + '/' + fileName)
     _image = _image.resize(SCALING_SIZE)
     os.system("rm -rf " + os.path.join(VAL_PATH, fileName))
     _image.save(os.path.join(VAL_PATH, fileName))
 print('Completed Validation images.....')
+
+end = time.time()
+print("Time taken to resize:", round((end - start), 2),"seconds")
