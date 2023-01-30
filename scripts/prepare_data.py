99232386e5bbde013530e541a2ad1517829cda3d
sameepv21 <sameep0112vani@gmail.com>
2023-01-30 16:00:32 +0530

    fix: successfully generated test annotations

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -33,9 +33,32 @@ def organize():
 # Create annotation.json for test dataset
 def generate_test_annotation(annotation_path = ANNOTATION_PATH):
     print("Generating Annotations File...")
+    test_json = []
+    train_json = []
     with open(annotation_path) as json_file:
         data = json.load(json_file)
 
+    temp_arr = []
+    for fileName in os.listdir('../data/test/images'):
+        temp_arr.append(int(fileName.strip('.jpg')))
+
+    for img_data in data:
+        if(img_data.get('image_id') in temp_arr):
+            test_json.append(img_data)
+        else:
+            train_json.append(img_data)
+    
+    # save to the respective locations
+    train_object = json.dumps(train_json)
+    test_object = json.dumps(test_json)
+
+    # save train object
+    with open('../data/train/annotation.json', 'w') as outfile:
+        outfile.write(train_object)
+    
+    # save test object
+    with open('../data/test/annotation.json', 'w') as outfile:
+        outfile.write(test_object)
 
 def main():
     split_images()
