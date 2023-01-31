0e8e5a7690e02e19451fbc4c99d5b3ff5921793e
sameepv21 <sameep0112vani@gmail.com>
2023-01-31 12:59:20 +0530

    bug: keypoints are still out of range

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -2,6 +2,8 @@
 import time
 from PIL import Image
 import os
+import numpy as np
+import json
 
 start = time.time()
 
@@ -9,41 +11,113 @@ start = time.time()
 TRAIN_PATH = "../data/train/images"
 TEST_PATH = "../data/test/images"
 VAL_PATH = '../data/valid/images'
+
+TRAIN_ANNOTATION_PATH = '../data/train/annotation.json'
+TEST_ANNOTATION_PATH = '../data/test/annotation.json'
+VAL_ANNOTATION_PATH = '../data/valid/annotation.json'
+
+TRAIN_DATA = json.load(open(TRAIN_ANNOTATION_PATH))
+VAL_DATA = json.load(open(VAL_ANNOTATION_PATH))
+TEST_DATA = json.load(open(TEST_ANNOTATION_PATH))
+
 SCALING_SIZE = (224, 224)
 
-print('Processing Train images.....')
-# Load and resize train image
-for index, fileName in enumerate(os.listdir(TRAIN_PATH)):
-    if index % 4000 == 0:
-        print('Processed', index, 'images')
-    _image = Image.open(TRAIN_PATH + '/' + fileName)
-    _image = _image.resize(SCALING_SIZE)
-    os.system("rm -rf " + os.path.join(TRAIN_PATH, fileName))
-    _image.save(os.path.join(TRAIN_PATH, fileName))
-print('Completed Train images.....')
+
+# print('Processing Train images.....')
+# # Load and resize train image
+# for index, fileName in enumerate(os.listdir(TRAIN_PATH)):
+#     if index % 4000 == 0: # Give progress update after every 4000 images
+#         print('Processed', index, 'images')
+
+#     image_id = fileName.strip('.jpg')
+#     _image = Image.open(TRAIN_PATH + '/' + fileName)
+#     image_np = np.asarray(_image)
+
+#     # find the new scaling factor after the image resize
+#     row_scaling_factor = SCALING_SIZE[0] / image_np.shape[0]
+#     col_scaling_factor = SCALING_SIZE[1] / image_np.shape[1]
+
+#     # fetch keypoints, bbox and segmentation
+#     keypoints = TRAIN_DATA.get(image_id).get('keypoints')
+#     bbox = TRAIN_DATA.get(image_id).get('bbox')
+
+#     # resize keypoints
+#     keypoints[:, 0] = keypoints[:, 0] * row_scaling_factor
+#     keypoints[:, 1] = keypoints[:, 1] * col_scaling_factor
+
+#     # resize bbox
+#     bbox[:, 0] = bbox[:, 0] * row_scaling_factor
+#     bbox[:, 1] = bbox[:, 1] * col_scaling_factor
+#     bbox[:, 2] = bbox[:, 2] * row_scaling_factor
+#     bbox[:, 3] = bbox[:, 3] * col_scaling_factor
+
+#     # save it to the json
+#     TRAIN_DATA.get(image_id)['keypoints'] = keypoints
+#     TRAIN_DATA.get(image_id)['bbox'] = bbox
+
+#     _image = _image.resize(SCALING_SIZE)
+#     os.system("rm -rf " + os.path.join(TRAIN_PATH, fileName))
+#     _image.save(os.path.join(TRAIN_PATH, fileName))
+
+# # save json to file
+# train_object = json.dumps(TRAIN_DATA)
+# with open(TRAIN_ANNOTATION_PATH, 'w') as outfile:
+#     outfile.write(train_object)
+# print('Completed Train images.....')
 
 
-print('\nProcessing Test images.....')
-# Load and resize test image
-for index, fileName in enumerate(os.listdir(TEST_PATH)):
-    if index % 4000 == 0:
-        print('Processed', index, 'images')
-    _image = Image.open(TEST_PATH + '/' + fileName)
-    _image = _image.resize(SCALING_SIZE)
-    os.system("rm -rf " + os.path.join(TEST_PATH, fileName))
-    _image.save(os.path.join(TEST_PATH, fileName))
-print('Completed Test images.....')
+# print('\nProcessing Test images.....')
+# # Load and resize test image
+# for index, fileName in enumerate(os.listdir(TEST_PATH)):
+#     if index % 4000 == 0: # Give progress update after every 4000 images
+#         print('Processed', index, 'images')
+#     _image = Image.open(TEST_PATH + '/' + fileName)
+#     _image = _image.resize(SCALING_SIZE)
+#     os.system("rm -rf " + os.path.join(TEST_PATH, fileName))
+#     _image.save(os.path.join(TEST_PATH, fileName))
+# print('Completed Test images.....')
 
 
 print('\nProcessing Validation images.....')
-# Load and resize validation image
+# Load and resize train image
 for index, fileName in enumerate(os.listdir(VAL_PATH)):
-    if index % 4000 == 0:
+    if index % 4000 == 0: # Give progress update after every 4000 images
         print('Processed', index, 'images')
+
+    image_id = fileName.strip('.jpg')
     _image = Image.open(VAL_PATH + '/' + fileName)
+    image_np = np.asarray(_image)
+
+    # find the new scaling factor after the image resize
+    row_scaling_factor = SCALING_SIZE[0] / image_np.shape[0]
+    col_scaling_factor = SCALING_SIZE[1] / image_np.shape[1]
+
+    # fetch keypoints, bbox and segmentation
+    keypoints = np.array(VAL_DATA.get(image_id).get('keypoints'))
+    bbox = np.array(VAL_DATA.get(image_id).get('bbox'))
+
+    # resize keypoints
+    keypoints[:, 0] = keypoints[:, 0] * row_scaling_factor
+    keypoints[:, 1] = keypoints[:, 1] * col_scaling_factor
+
+    # resize bbox
+    bbox[0] = bbox[0] * row_scaling_factor
+    bbox[1] = bbox[1] * col_scaling_factor
+    bbox[2] = bbox[2] * row_scaling_factor
+    bbox[3] = bbox[3] * col_scaling_factor
+
+    # save it to the json
+    VAL_DATA.get(image_id)['keypoints'] = keypoints.tolist()
+    VAL_DATA.get(image_id)['bbox'] = bbox.tolist()
+
     _image = _image.resize(SCALING_SIZE)
     os.system("rm -rf " + os.path.join(VAL_PATH, fileName))
     _image.save(os.path.join(VAL_PATH, fileName))
+
+# save json to file
+val_object = json.dumps(VAL_DATA)
+with open(VAL_ANNOTATION_PATH, 'w') as outfile:
+    outfile.write(val_object)
 print('Completed Validation images.....')
 
 end = time.time()
